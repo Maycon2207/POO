3458c69f2c3386d5f8255763cb06ce57f6bdcf27
Maycon Douglas <maycondouglas9292senac@gmail.com>
2025-09-12 19:24:39 -0300

    pronto

---
+++ 02_Construtores/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Construtores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // sem o filtro do construtor
+            Quadrado quadrado = new Quadrado();
+            quadrado.ImprimeArea();
+
+            Quadrado quadrado2 = new Quadrado(10);
+            quadrado2.ImprimeArea();
+
+            Retangulo retangulo = new Retangulo(5, 6);
+            retangulo.ImprimeArea();
+
+            Circulo obj4 = new Circulo(4);
+            obj4.ImprimeArea();
+
+            Triangulo obj5 = new Triangulo(4, 5);
+            obj5.ImprimeArea();
+
+
+        }
+        
+       
+    }
+    public class Quadrado
+    {
+        //modificado de acesso private
+        //pode ser acessado apenas dentro da classe
+        private int lado;   // como estou usando private la em cima nao deixa puxar entao tenho que usar o construtor 
+
+        public Quadrado(int Lado)
+        {
+            this.lado = Lado;
+        }
+        //overload ( sobrecarga ) outro modo de contrutor um recebe parametro outro nao
+        //declarar mais de um contrutor com a lista de argumento diferentes
+        //this(5) acima o contrutor que recebe um parametro.
+        public Quadrado():this(5)
+        {
+           // this.lado = 5;
+        }
+        public int CalculaArea()
+
+        {
+            return lado * lado;
+            //o contrutor serve para instanciar o obejto e inicializar os atributos
+        }
+        public void ImprimeArea()
+        {
+            Console.WriteLine($"Quadrado com lado de {lado} possui uma area de {CalculaArea()}");
+
+        }
+        //this - referencia o objeto atual
+    }
+    public class Retangulo
+    {
+        private int largura;
+        private int altura;
+
+        public Retangulo(int Largura, int Altura)
+        {
+            this.largura = Largura;
+            this.altura = Altura;
+        }
+        public int CalculaArea()
+        {
+            return largura * altura;
+
+        }
+        public void ImprimeArea()
+        {
+            Console.WriteLine($"Retangulo com largura de {largura} e altura de {altura} possui uma area de {CalculaArea()}");
+        }
+
+    }
+    public class Circulo
+    {
+        private double raio;
+        private const double pi = 3.14;
+        public Circulo(double Raio)
+        {
+            this.raio = Raio;
+        }
+        public double CalculaArea()
+        {
+            return pi * (raio * raio);
+        }
+        public void ImprimeArea()
+        {
+            Console.WriteLine($"Circulo com raio de {raio} possui uma area de {CalculaArea()}");
+        }
+    }
+    public class Triangulo
+    {
+        private int base1;
+        private int altura;
+        public Triangulo(int Base, int Altura)
+        {
+            this.base1 = Base;
+            this.altura = Altura;
+        }
+        public double CalculaArea()
+        {
+            return (base1 * altura) / 2.0;
+        }
+        public void ImprimeArea()
+        {
+            Console.WriteLine($"Triangulo com base de {base1} e altura de {altura} possui uma area de {CalculaArea()}");
+        }
+    }
+} 
+
+//sempre que for private a inicial e minuscula 
+
+
+
+
+
+
+
---
+++ 03)Propiedades/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace _03_Propiedades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Conta conta = new Conta();
+            Conta conta = new Conta("12345-X");
+            conta.setNome("Celso Serrano Araujo");
+            // conta.Numero = "12345-X";
+            //conta.saldo = -1000
+            conta.Depositar(-1000.00m);
+
+            //utilizando metodo acessador
+            conta.setSaldo(1000.00m);
+            //Console.WriteLine($"Saldo atual. {conta.getSaldo():c2}");
+            conta.imprimirSaldo();
+            while (true)
+
+            {
+                
+                Console.WriteLine("informe a Operação: [D]-depositar, [S]-sacar ou [E]-sair");
+                string operacao = Console.ReadLine();
+
+                if (operacao.ToUpper() == "D")
+                {
+                    Console.WriteLine("informe o valor do deposito:");
+                    decimal valorDeposito = decimal.Parse(Console.ReadLine());
+                    conta.Depositar(valorDeposito);
+                    conta.imprimirSaldo();
+                }
+                else if (operacao.ToUpper() == "S")
+                {
+                    Console.WriteLine("informe o valor para saque:");
+                    decimal valorSaque = decimal.Parse(Console.ReadLine());
+                    conta.Sacar(valorSaque);
+                    conta.imprimirSaldo();
+
+                }
+                else if (operacao.ToUpper() == "E")
+                {
+                    Console.WriteLine($"conta: {conta.Nome} - {conta.getSaldo():c2}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Informe Apenas as LEtras [D] para depositar, [S] para sacar ou [E] para sair");
+                }
+
+
+            }    
+        }
+
+    }
+    public class Conta
+    {
+        private string numero;
+        private string nome;
+
+
+        //tranformar a conta saldo com acesso privado
+        // public decimal saldo;
+        private decimal saldo;
+        public Conta(string numero)
+        {
+            this.numero = numero;
+        }
+        private string setnome;
+
+        public string getNome()
+        {
+            return nome;
+        }
+
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+
+        public decimal getSaldo()
+        {
+            return saldo;
+        }
+
+        public void setSaldo(decimal saldo)
+        {
+            this.Depositar(saldo);
+        }
+
+        public string getNumero()
+        {
+            return numero;
+        }
+
+        public void Depositar(decimal valor)
+        {
+            if (valor > 0)
+                saldo += valor;
+
+            else
+                Console.WriteLine("Valor invalido para deposito o valor deve ser maior que Zero");
+
+
+        }
+        public void Sacar(decimal valor)
+        {
+            if (valor > 0 && valor <= saldo)
+                saldo -= valor;
+            else
+                Console.WriteLine("Valor invalido para saque o valor deve ser maior que Zero e menor que o saldo atual");
+            
+        }
+
+        public void imprimirSaldo()
+        {
+            Console.WriteLine($"Saldo atual. {getSaldo():c2}");
+        }
+    }
+}
