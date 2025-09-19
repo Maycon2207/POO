31627bb46251a8409c72771ac101d682cf6948f1
Maycon Douglas <maycondouglas9292senac@gmail.com>
2025-09-19 20:42:17 -0300

    heranças conteudo completo

--- 09_heranca/Pessoa.cs
+++ 09_heranca/Pessoa.cs
@@ -8,6 +8,7 @@ namespace _09_heranca
 {
     internal  abstract class Pessoa
     {
+        public int Id { get; set; }
         public string Nome;
         public int telefone;
 
@@ -17,6 +18,22 @@ namespace _09_heranca
         {
             Console.WriteLine($"\nLISTA DE PESSOA\n\nnome:{this.Nome} Documento:{this.GetDocumento()} Telefone:{this.telefone}");
         }
-
+        public override string ToString()
+        {
+            return $"<toString()>nome:{this.Nome} Documento:{this.GetDocumento()} Telefone:{this.telefone}";
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null /*|| this.GetType() != obj.GetType()*/)
+            
+                return false;
+            //Cast=> conversao de objeto
+            //((pessoa)obj).Id => converte obj que e do tipo object para o tipo pessoa
+            return this.Id == ((Pessoa)obj).Id;
+        }
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();  
+        }
     }
 }
--- 09_heranca/Program.cs
+++ 09_heranca/Program.cs
@@ -12,17 +12,40 @@ namespace _09_heranca
         {
             //Nao e possivel instanciar uma classe abstrata
             //pessoa obj = new pessoa();
+
+            Pessoa[] pessoas = new Pessoa[2];
+
+
             PessoaFisica pessoaFisica = new PessoaFisica();
+            pessoas[0] = pessoaFisica;
+            pessoaFisica.Id = 1;
             pessoaFisica.Nome = "Geraldo";
             pessoaFisica.Cpf = "123.456.789-00";
             pessoaFisica.telefone = 123456789;
             pessoaFisica.Imprimir();
 
             PessoaJuridica pessoaJuridica = new PessoaJuridica();
+            pessoas[1] = pessoaJuridica;
+            pessoaJuridica.Id = 1;
             pessoaJuridica.Nome = "Empresa LTDA";
             pessoaJuridica.Cnpj = "12.345.678/0001-00";
             pessoaJuridica.telefone = 987654321;
             pessoaJuridica.Imprimir();
+
+            Console.WriteLine(pessoaFisica);
+            Console.WriteLine(pessoaJuridica);
+
+            if (pessoaFisica.Equals(pessoaJuridica))
+                Console.WriteLine("É a mesma pessoa");
+            else
+                Console.WriteLine("Não é a mesma pessoa");
+
+            foreach (Pessoa pessoa in pessoas)
+            {
+                Console.WriteLine(pessoa);
+            }
+           
         }
+
     }
 }
