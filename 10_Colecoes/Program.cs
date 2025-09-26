b7bf83b02d004c6c892ccf14fdec456854af3688
Maycon Douglas <maycondouglas9292senac@gmail.com>
2025-09-25 21:49:30 -0300

    pronto

--- 10_Colecoes/Program.cs
+++ 10_Colecoes/Program.cs
@@ -38,6 +38,70 @@ namespace _10_Colecoes
                 aluno.ImprimirAluno();
             }
 ;
+            Dictionary<int, String> dicNomes = new Dictionary<int, String>();
+            dicNomes.Add(1, "Gustavo");
+            dicNomes.Add(2, "paulo");
+            dicNomes.Add(3, "maycon");
+
+            Console.WriteLine("impressão dos dicNomes");
+            for (int i = 1; i <= dicNomes.Count; i++)
+            {
+                Console.WriteLine($"- {dicNomes[i]}");
+            }
+            Dictionary<int, Aluno> dicAlunos = new Dictionary<int, Aluno>();
+            dicAlunos.Add(maycon.id, maycon);
+            dicAlunos.Add(paulo.id, paulo);
+
+            Console.WriteLine("impressão dos dicAlunos");
+           // for (int i = 1; i <= dicAlunos.Count; i++)
+        //    {
+         //       dicAlunos[i].ImprimirAluno();
+         //   }
+         foreach (Aluno aluno in dicAlunos.Values)
+            {
+                aluno.ImprimirAluno();
+            }
+
+         Queue<string> filaNomes = new Queue<string>();
+            filaNomes.Enqueue("Gustavo");
+            filaNomes.Enqueue("paulo");
+            filaNomes.Enqueue("maycon");
+
+            Console.WriteLine("Impressao dos filaNomes");
+            Console.WriteLine($" 1º) {filaNomes.Dequeue()}");
+
+            foreach (string nome in filaNomes)
+            {
+                Console.WriteLine($"- {nome}");
+            }
+
+            Stack<string> stackNomes = new Stack<string>();
+            stackNomes.Push("gustavo");
+            stackNomes.Push("paulo");
+            stackNomes.Push("maycon");
+
+            Console.WriteLine($"Impressão dos stackNomes");
+            Console.WriteLine($" 1º ){stackNomes.Pop()}");
+
+            foreach (string nome in stackNomes)
+            {
+                Console.WriteLine($"- {nome}");
+            }
+
+            HashSet<string> setNomes = new HashSet<string>();
+            setNomes.Add("gustavo");
+            setNomes.Add("paulo");
+            setNomes.Add("maycon");
+            setNomes.Add("gustavo");
+            setNomes.Add("paulo");
+            setNomes.Add("maycon");
+
+
+            Console.WriteLine("impressao dos setNomes");
+            foreach (string nome in setNomes)
+            {
+                Console.WriteLine($"- {nome}");
+            }
 
 
 
@@ -67,7 +131,16 @@ namespace _10_Colecoes
          Console.WriteLine($" ID: {this.id} Nome:{this.Nome} - {this.Deficiencia} ");
         }
 
+     
+
 
 
     }
 }
+//concolusao 
+//cole~ção sao mais flexiveis que  
+//cada tipo setrve a uma necessidade
+//list > lista ordena com dúplicadas 
+//dictionary > chave e valor
+// queue > fila FIFO
+//Stack > pilha FIFO 
